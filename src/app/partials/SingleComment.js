3d5c8bab0f65afe83ae7aa6904188deb6a5e2b5f
BIT Student <classroom@bgit.rs>
2018-06-19 17:06:13 +0200

    final

--- src/app/partials/SingleComment.js
+++ src/app/partials/SingleComment.js
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { fetchUser } from '../../services/userService';
 
 
+export class SingleComment extends Component{
+    constructor(props){
+        super(props),
 
-export const SingleComment = (props) => {
+        this.state = {
+            user: {}
+        }
+    }
 
-    return (
-        <div className="collection-item avatar comment">
-            <img src="https://varme-center.se/wp-content/uploads/2016/06/omdome.png" alt="" className="circle" />
-            <span className="title username">{props.comment.authorName}</span>
-            <p>{props.comment.body}
-            </p>
-        </div>
-    )
+    fetchUserData(){
+        fetchUser(this.props.comment.authorId)
+        .then((user) => {
+            this.setState({user:user})
+        })
+    }
+
+    componentDidMount(){
+        this.fetchUserData();
+    }
+
+    render(){
+        return (
+            <div className="collection-item avatar comment">
+                <img src={this.state.user.avatarUrl} alt="" className="circle" />
+                <span className="title username">{this.props.comment.authorName}</span>
+                <p>{this.props.comment.body}
+                </p>
+            </div>
+        ) 
+
+    }
+
+    
 }
 
